eba156bf53f5072b0334d11e0ac6040eda1b0bfb
NimraMushtaq <77542584+NimraMushtaq@users.noreply.github.com>
2021-08-24 13:56:47 +0500

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,15 @@
+(function(){
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for ( var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter == 'j') {
+    // byeSpeaker.xxxx
+    byeSpeaker.speak(names[i]);
+   } 
+   else {
+    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
