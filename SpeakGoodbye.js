7f7302f9535c6d5596c28f5c050df1833b2b0531
NimraMushtaq <77542584+NimraMushtaq@users.noreply.github.com>
2021-08-24 14:13:01 +0500

    Delete SpeakGoodbye.js

--- SpeakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-var helloSpeaker={};
-var speakWord = "Good Bye";
-helloSpeaker.speak=function(name) {
-  console.log(speakWord + " " + name);
-}	
-window.helloSpeaker=helloSpeaker;
-})(window);
